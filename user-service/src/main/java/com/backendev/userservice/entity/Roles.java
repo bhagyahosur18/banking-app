c3d581fc7d9a6fdbbb76dc4f22a423d611b12f25
bhagyahosur18 <bhagyashreehosur@gmail.com>
2025-09-04 15:51:30 +1000

    refactor: Added additional fields to /login response

--- user-service/src/main/java/com/backendev/userservice/entity/Roles.java
+++ user-service/src/main/java/com/backendev/userservice/entity/Roles.java
@@ -10,12 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "roles")
-public class Roles {
+public class Roles implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
